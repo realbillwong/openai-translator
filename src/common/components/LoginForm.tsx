2239a0eb38d84006898eac194c6f9e20f9217ebf
Bill <null@gmail.com>
2024-01-26 21:18:58 +0800

    feat: add signin link

--- src/common/components/LoginForm.tsx
+++ src/common/components/LoginForm.tsx
@@ -11,12 +11,13 @@ import { Button as BaseButton } from 'baseui-sd/button'
 import { useCallback, useState, useEffect } from 'react'
 import { gptEditService, ILogin } from '../internal-services/gptedit'
 import { IThemedStyleProps, IUserInfo } from '../types'
+import { StyledLink } from 'baseui-sd/link'
 
 const { Form, FormItem } = createForm<ILogin>()
 
 const useStyles = createUseStyles({
     login: (props: IThemedStyleProps) => ({
-        padding: '50px 100px',
+        padding: '20px 100px 50px',
         background: props.themeType === 'dark' ? 'rgba(31, 31, 31, 1)' : 'rgba(255, 255, 255, 1)',
     }),
 })
@@ -83,9 +84,17 @@ export function LoginForm({ engine, onLogin }: ILoginFormProps) {
                         <FormItem required name='password' label={t('Password')}>
                             <BaseInput size='compact' type='password' />
                         </FormItem>
-                        <BaseButton isLoading={loading} size='compact'>
-                            {t('Submit')}
+                        <BaseButton
+                            style={{ display: 'block', width: '100%', marginBottom: '20px' }}
+                            isLoading={loading}
+                            size='compact'
+                        >
+                            {t('Login')}
                         </BaseButton>
+                        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                            <StyledLink href='https://gpt4edit.com/signin'>立即注册</StyledLink>
+                            <StyledLink href='https://gpt4edit.com/forgot-password'>忘记密码？</StyledLink>
+                        </div>
                     </Form>
                 </BaseProvider>
             </StyletronProvider>
